c466b390f6aff63af67217e6766eb57ba1fc6dca
1hue <212147609+1hue@users.noreply.github.com>
2025-09-27 17:03:21 +0100

    Update code comments in `glsl`

--- src/compute_shader.glsl
+++ src/compute_shader.glsl
@@ -8,7 +8,7 @@ layout(local_size_x = 4, local_size_y = 2) in;
 layout(set = 0, binding = 0, std430) buffer DataStorageBuffer {
   // Order here is very important for the right byte offsets outside of the shader
   uint counter;
-  // Layout rules dictate that this must be a group of 16 bytes (=4 floats), but we can omit/comment out the following line as it can be inferred
+  // std430 layout rules dictate that this must have a boundary every 16 bytes (=4 floats), but we can omit/comment out the following line as it can be inferred
   // float padding;
   vec2 constants;
 
@@ -22,14 +22,14 @@ void main() {
   uint idx = gl_LocalInvocationIndex;
   uint prev = atomicAdd(counter, 1u);
 
-  // Each invocation is uniquely identified by idx and only touches that particular array index
+  // Each invocation is uniquely identified by idx and touches only its own corresponding array index
   storage_data[idx] = push_data[idx];
 
   // With our persistent storage buffer, the data doesn't disappear until we destroy the buffer ourselves,
   // so we could keep adding:
   //storage_data[idx] += push_data[idx];
 
-  // Don't need to run this 8 times, only run it on invocation 0
+  // Only run this on invocation 0, don't need to run it 8 times
   if (idx == 0) {
     constants = vec2(CONSTANT_0, CONSTANT_1);
   }
